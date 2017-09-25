2d2843b5abf65ec01ec8de2008ff844e10556e4d
yizhucannotdrive <32277768+yizhucannotdrive@users.noreply.github.com>
2017-09-25 16:37:36 -0500

    Add files via upload

---
+++ PID.cpp
@@ -0,0 +1,28 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
+}
+
+void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+}
+
+double PID::TotalError() {
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
+}
+
